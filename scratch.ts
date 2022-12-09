da9a713ec17093dc4090e6d054dbeef8f7c2fae5
Jayaram Anandan <68646971+jayaramanandan@users.noreply.github.com>
2022-12-09 12:51:49 +0000

    Update scratch.ts

--- scratch.ts
+++ scratch.ts
@@ -1,12 +1,13 @@
-import fetch from "node-fetch";
+import get, { AxiosResponse } from "axios";
 
 class Scratch {
-  public async getProjects(): Promise<any> {
-    const res = await fetch("https://scratch.mit.edu/site-api/projects/all/", {
-      headers: { method: "GET" },
-    });
-    return;
+  public async login(): Promise<boolean> {
+    const res: AxiosResponse<any, any> = get("https://scratch.mit.edu/csrf_token/", {
+      headers:{
+        "Cookie": "_ga=GA1.3.155267336.1670589494; _gid=GA1.3.1776564626.1670589494; permissions=%7B%7D"
+      }
+    })
+    
+    console.log(res)
   }
 }
-
-export default Scratch;
